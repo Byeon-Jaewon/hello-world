5a87480129c1fb92afd2bb41d268ac1126c602a0
Byeon-Jaewon <bjw5095@gmail.com>
2020-03-21 22:47:11 +0900

    7568덩치 답안

--- 11/size.py
+++ 11/size.py
@@ -1,22 +1,11 @@
 N=int(input()) 
-cm, kg=[],[]
-kre=[0]*N ; cre=[0]*N
-count=1
-for i in range(N):
-    x,y = map(int,input().split())
-    kg.append(x)
-    cm.append(y)
-print(cm, kg)
-while 0 in kre :
-    kre[kg.index(max(kg))]=count
-    count+=1 ; kg[kg.index(max(kg))]=0
-count=1
-while 0 in cre :
-    cre[cm.index(max(cm))]=count
-    count+=1 ; cm[cm.index(max(cm))]=0
-for i in range(N) :
-    if cre[i]==kre[i] : 
-        print(cre[i], end=' ')
-    else :
-        print(2, end=' ')
-
+people=[]
+for _ in range(N):
+    kg, cm = map(int,input().split())
+    people.append((kg,cm))
+for i in range(N) : 
+    rank=1
+    for j in range(N):
+        if people[i][0]<people[j][0] and people[i][1]<people[j][1] :
+            rank+=1
+    print(rank,end=' ')

09ed745693437c6fce1a3c8357ccd0596abce62b
Byeon-Jaewon <bjw5095@gmail.com>
2020-03-30 23:31:43 +0900

    실패

--- 10/star-10.py
+++ 10/star-10.py
@@ -1,3 +1,15 @@
-def star(n):
-    
-
+A="***"
+B="* *"
+C="   "
+def star(i,j) :
+    if j%3==1 and i//3%3==1:
+        print(C,end='')
+    elif i%3==1:
+        print(B,end='')
+    else :
+        print(A,end='')
+N=int(input())
+for i in range(N):
+    for j in range(N//3):
+        star(i,j)
+    print("")

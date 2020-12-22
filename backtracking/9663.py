308ae8b6d40cd9deb5d96c3a01f0b43ee444e041
Byeon-Jaewon <bjw5095@gmail.com>
2020-12-22 21:44:50 +0900

    시간초과가 되네요...

--- backtracking/9663.py
+++ backtracking/9663.py
@@ -1,31 +1,15 @@
 N = int(input())
-lst=[]
-MAX = N*N
-count = 0
-for i in range(0, MAX) :
-    lst.append(False)
-def init(lst):
-    for i in range(MAX):
-        lst[i]=False
-def queen(lst, point) :
-    for i in range(0, MAX):
-        if(int(i/N)==(int(point/N))) :
-            lst[i]=True
-        if((i%N)==(point%N)):
-            lst[i]=True
-        if((i%N)+int(i/N)==(point%N)+int(point/N)):
-            lst[i]=True
-        if(int(i/N)-(i%N)==(int(point/N)-(point%N))):
-            lst[i]=True
-def solve(ind):
-    if ind == N :
-        count+=1
-        return
-    for i in range(MAX):
-        if lst[i]==True:
-            continue
-        queen(lst, i)
-        solve(ind+1)
+a, b, c = [False]*N, [False]*(2*N-1), [False]*(2*N-1)
 
-solve(0)
-print(count)
+
+def solve(i):
+    if i == N:
+        return 1
+    result = 0
+    for j in range(N):
+        if not (a[j] or b[i+j] or c[i-j+N-1]):
+            a[j] = b[i+j]= c[i-j+N-1] = True
+            result += solve(i+1)
+            a[j] = b[i+j]= c[i-j+N-1] = False
+    return result
+print(solve(0))

a4893002ff018d3faacb719884ea224f3d977aac
Byeon-Jaewon <bjw5095@gmail.com>
2021-02-10 23:56:17 +0900

    read vertical

--- string/10798.py
+++ string/10798.py
@@ -1,5 +1,8 @@
 lst = []
 for i in range(5):
     lst.append(input())
-for i in range(5):
-    for j in lst:
+for i in range(0,15):
+    for j in range(0, 5):
+        if i >= len(lst[j]) :
+            continue
+        print(lst[j][i], end='')

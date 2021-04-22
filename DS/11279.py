36e85757b29be54774c4d51a10761c89e4cc2c3b
Byeon-Jaewon <bjw5095@gmail.com>
2021-04-22 15:57:17 +0900

    max_heap

---
+++ DS/11279.py
@@ -0,0 +1,22 @@
+import heapq
+import sys
+
+input = lambda : sys.stdin.readline()
+
+def maxheap_push(x):
+    heapq.heappush(heap, (-x, x))
+def maxheap_pop():
+    if len(heap) == 0 :
+        print(0)
+    else :
+        max_item = heapq.heappop(heap)[1]
+        print(max_item)
+
+heap = []
+N = int(input())
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        maxheap_pop()
+    else :
+        maxheap_push(x)
